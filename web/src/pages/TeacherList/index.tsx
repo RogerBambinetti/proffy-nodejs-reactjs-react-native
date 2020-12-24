c9852a4ec5c9c1dd69e6d6bfb36389bf3a84f6d5
Roger Bambinetti <rogerbambinetti@hotmail.com>
2020-12-23 23:20:22 -0300

    update frontend

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import Input from '../../components/Input';
 import PageHeader from '../../components/PageHeader';
 import TeacherItem from '../../components/TeacherItem';
 
@@ -10,21 +11,11 @@ export default function TeacherList() {
         <div id="page-teacher-list" className="container">
             <PageHeader title="Esses são os proffys disponíveis.">
                 <form id="search-teachers">
-                    <div className="input-block">
-                        <label htmlFor="subject">Matéria</label>
-                        <input type="text" id="subject" />
-                    </div>
+                    <Input name="subject" label="Matéria" />
 
-                    <div className="input-block">
-                        <label htmlFor="week_day">Dia da Semana</label>
-                        <input type="text" id="week_day" />
-                    </div>
+                    <Input name="week_day" label="Dia da Semana" />
 
-                    <div className="input-block">
-                        <label htmlFor="time">Hora</label>
-                        <input type="text" id="time" />
-                    </div>
-
+                    <Input type="time" name="time" label="Hora" />
                 </form>
             </PageHeader>
             <main>
